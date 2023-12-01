093700bcefe1ec2b1bc86ed442dd6069a16ac0a0
ostifede02 <ostifederico02@gmail.com>
2023-12-01 10:29:20 -0500

    code clean

--- script/main.py
+++ script/main.py
@@ -8,30 +8,28 @@ from kalman_filter import KalmanFilter
 import configuration as conf
 
 
-# ************  object detection  ************
-video_path = os.path.join(os.path.dirname(__file__), '../sources/videos/test_car.mp4')
-od = ObjectDetection(video_path)
-
-# ************  Kalman filter  ************
-kf = KalmanFilter(conf.A, conf.B, conf.C, conf.Q, conf.R, conf.x, conf.u, conf.dt)
-
-measured = []
-predicted = []
-mp = np.zeros((2, 1), np.float32)   # measured position
-tp = np.zeros((4, 1), np.float32)   # predicted position
-
-
 def main():
+    # ************  object detection  ************
+    video_path = os.path.join(os.path.dirname(__file__), '../sources/videos/test_car.mp4')
+    od = ObjectDetection(video_path)
 
+    # ************  Kalman filter  ************
+    kf = KalmanFilter(conf.A, conf.B, conf.C, conf.Q, conf.R, conf.x, conf.u, conf.dt)
+
+    measured = []
+    predicted = []
+    mp = np.zeros((2, 1), np.float32)   # measured position
+    tp = np.zeros((4, 1), np.float32)   # predicted position    
+    
     while True:
-        frame = od.read((0.6, 0.6))   
-        mp = od.pose_estimation(frame)
-        if mp is None:
-            # print("Object identified = None")
-            continue
+        frame = od.read(resize=(0.6, 0.6))      # get frame from imported video
 
-        kf.update(mp)
-        tp = kf.predict()
+        mp = od.pose_estimation(frame)          # measure position of marker
+        if mp is None:
+            continue                            # if not detected, go to next frame
+        
+        kf.update(mp)                           # update the Kalman filter with new measurement
+        tp = kf.predict()                       # predict next position
 
         # plot states and frame
         measured.append(mp)
